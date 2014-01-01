579ae1d8c844b7f06395c6acd815732f1242a1af
dingziran <dingziranrr@163.com>
2014-01-01 23:36:54 +0800

    modify the structure to add angularjs and bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,17 +4,14 @@ import play.api._
 import play.api.mvc._
 import play.api.data.{Forms, Form}
 import play.api.data.Forms._
-import models.User
 
 object Application extends Controller {
 
   def index = Action {
-    Redirect(auth.routes.Login.index)
-    //Ok(views.html.auth.login())
-    //Ok(views.html.index("Your new application is ready."))
-  }
-  def help=Action{
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def activities =Action{
+      Ok(views.html.activities())
+  }
 }
---
+++ public/javascripts/activities/controllers.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dingziran
+ * Date: 14-1-1
+ * Time: 下午9:54
+ * To change this template use File | Settings | File Templates.
+ */
